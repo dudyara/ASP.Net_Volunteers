357bafca66321ad0eabe5f20f3ea4b118e72a183
Ruslan <ruslan-karamurzanov@mail.ru>
2021-05-26 21:32:08 +0700

    Исправление RequestStatus

--- Volunteers/Volunteers.Entities/Enums/RequestStatus.cs
+++ Volunteers/Volunteers.Entities/Enums/RequestStatus.cs
@@ -1,24 +1,21 @@
 ﻿namespace Volunteers.Entities.Enums
 {
+    using System.ComponentModel;
     /// <summary>
     /// Статус заявки.
     /// </summary>
     public enum RequestStatus : long
     {
-        /// <summary>Приоритет не определен.</summary>
-        [Description("Не определен")]
-        Undefined = 0,
+        /// <summary>Ожидание рассмотрения.</summary>
+        [Description("Ожидание рассмотрения")]
+        Waiting = 1,
 
-        /// <summary>Высокий приоритет.</summary>
-        [Description("Высокий приоритет")]
-        High = 1,
+        /// <summary>Ожидание исполнения.</summary>
+        [Description("Ожидание исполнения")]
+        Execution = 2,
 
-        /// <summary>Средний приоритет.</summary>
-        [Description("Средний приоритет")]
-        Middle = 2,
-
-        /// <summary>Низкий приоритет.</summary>
-        [Description("Низкий приоритет")]
-        Low = 3
+        /// <summary>Заявка завершена.</summary>
+        [Description("Заявка завершена")]
+        Done = 3
     }
 }
