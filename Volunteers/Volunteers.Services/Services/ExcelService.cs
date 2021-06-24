5a02c468ae7ae962e1bb6eab21ba288e6588a199
aleksandr.kargin <58880905+dudyara@users.noreply.github.com>
2021-06-24 18:03:25 +0700

    Добавлен эксель сервис

---
+++ Volunteers/Volunteers.Services/Services/ExcelService.cs
@@ -0,0 +1,24 @@
+﻿namespace Volunteers.Services.Services
+{
+    using Volunteers.DB;
+    using Volunteers.Entities;
+    using Volunteers.Services.Dto;
+    using Volunteers.Services.Mapper;
+
+    /// <summary>
+    /// ExcelService
+    /// </summary>
+    public class ExcelService : BaseService<Request, RequestDto>
+    {
+        /// <summary>
+        /// Метод заявок.
+        /// </summary>
+        /// <param name="mapper">Маппер.</param>
+        /// <param name="repository">repository</param>
+        /// <param name="validator">validator</param>
+        public ExcelService(IVolunteerMapper mapper, IDbRepository<Request> repository, IDtoValidator validator)
+            : base(mapper, repository, validator)
+        {
+        }
+    }
+}
