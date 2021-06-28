6955f5eb92e958047980b8244f3f9f416f9d2f7d
РУсик <ruslan-karamurzanov@mail.ru>
2021-06-28 18:16:10 +0700

    Перенесены роли в enum

---
+++ Volunteers/Volunteers.Entities/Enums/Roles.cs
@@ -0,0 +1,18 @@
+﻿namespace Volunteers.Entities.Enums
+{
+    /// <summary>
+    /// Roles
+    /// </summary>
+    public class Roles
+    {
+        /// <summary>
+        /// Admin
+        /// </summary>
+        public const string Admin = "Admin";
+
+        /// <summary>
+        /// Organization
+        /// </summary>
+        public const string Organization = "Organization";
+    }
+}
