f4646ef28928d66a4a622fdcf3b76da716e563a6
Yuriy Parshin <helltrial@gmail.com>
2021-07-01 14:53:37 +0700

    fix

--- Volunteers/Volunteers.Services/Services/OrganizationService.cs
+++ Volunteers/Volunteers.Services/Services/OrganizationService.cs
@@ -1,13 +1,9 @@
-﻿
-namespace Volunteers.Services.Services
+﻿namespace Volunteers.Services.Services
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
     using AutoMapper.QueryableExtensions;
-    using FluentValidation;
-    using Microsoft.AspNetCore.Http.Features;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
     using Volunteers.DB;
@@ -21,10 +17,10 @@ namespace Volunteers.Services.Services
     /// </summary>
     public class OrganizationService : BaseService<Organization, OrganizationDto>
     {
-
         private readonly RequestService _requestService;
         private readonly IDbRepository<Request> _requestRepo;
         private readonly IDbRepository<User> _userRepo;
+
         /// <summary>
         /// OrganizationService.
         /// </summary>
@@ -33,14 +29,14 @@ namespace Volunteers.Services.Services
         /// <param name="validator">validator</param>
         /// <param name="requestRepo">requestRepo</param>
         /// <param name="userRepo">userRepo</param>
-        /// <param name="requestService">requsetService</param>
+        /// <param name="requestService">requestService</param>
         public OrganizationService(
             IVolunteerMapper mapper,
             IDbRepository<Organization> repository,
-            IDtoValidator validator, 
-            IDbRepository<Request> requestRepo, 
+            IDtoValidator validator,
+            IDbRepository<Request> requestRepo,
             IDbRepository<User> userRepo,
-            [FromServices] RequestService requestService)
+            RequestService requestService)
             : base(mapper, repository, validator)
         {
             _requestRepo = requestRepo;
@@ -64,7 +60,7 @@ namespace Volunteers.Services.Services
         /// Create
         /// </summary>
         /// <param name="orgDto">org.</param>
-        /// <param name="id">user id</param>
+        /// <param name="id">user organizationId</param>
         public async Task<ActionResult<OrganizationDto>> Create(OrganizationDto orgDto, long id)
         {
             var org = Mapper.Map<Organization>(orgDto);
@@ -79,7 +75,6 @@ namespace Volunteers.Services.Services
         /// ChangeLogo
         /// </summary>
         /// <param name="logo">logo</param>
-        /// <returns></returns>
         public async Task<ActionResult<OrganizationDto>> ChangeLogo(OrganizationLogoDto logo)
         {
             var org = await Repository
@@ -97,8 +92,7 @@ namespace Volunteers.Services.Services
         /// <summary>
         /// Выдача организаций по типы их активности
         /// </summary>
-        /// <param name="ids">id активность</param>
-        /// <returns></returns>
+        /// <param name="ids">organizationId активность</param>
         public async Task<ActionResult<List<OrganizationDto>>> GetByIds(List<long> ids)
         {
             var organizationDtos = await Repository
@@ -112,36 +106,35 @@ namespace Volunteers.Services.Services
         /// <summary>
         /// Delete
         /// </summary>
-        /// <param name="id">id</param>
-        /// <param name="requestService">requestService</param>
-        /// <param name="userService">userService</param>
-        /// <returns></returns>
-        public async Task<ActionResult<Organization>> Delete(long id)
+        /// <param name="organizationId">organizationId</param>
+        public async Task<ActionResult<Organization>> Delete(long organizationId)
         {
-            // находим заявки компании
-            var allRequests = await _requestRepo.Get().Where(x => x.OrganizationId == id)
-                .Where(n => n.RequestStatus == RequestStatus.Execution).ToListAsync();
+            // находим заявки организации
+            var allRequests = await _requestRepo
+                .Get(x => x.OrganizationId == organizationId &&
+                          x.RequestStatus == RequestStatus.Execution)
+                .ToListAsync();
 
-            // меняес статус
+            // меняем статус
             foreach (var request in allRequests)
             {
-                await _requestService.ChangeStatus(new RequestChangeStatusDto()
+                await _requestService.ChangeStatus(new RequestChangeStatusDto
                 {
                     RequestId = request.Id,
                     RequestStatus = RequestStatus.Waiting
                 });
             }
 
-            // удаляем компанию
+            // удаляем организацию
             var org = await Repository
                 .Get()
-                .FirstOrDefaultAsync(x => x.Id == id);
+                .FirstOrDefaultAsync(x => x.Id == organizationId);
             org.UserId = null;
 
             // получает пользователя по компании
-            var userId = await Repository.Get().Where(x => x.Id == id).Select(t => t.UserId).FirstOrDefaultAsync(); 
-            await DeleteAsync(id);
-            await _userRepo.DeleteAsync((long)userId); 
+            var userId = await Repository.Get().Where(x => x.Id == organizationId).Select(t => t.UserId).FirstOrDefaultAsync();
+            await DeleteAsync(organizationId);
+            await _userRepo.DeleteAsync((long)userId);
             return org;
         }
 
@@ -149,7 +142,6 @@ namespace Volunteers.Services.Services
         /// Update
         /// </summary>
         /// <param name="orgDto">orgDto</param>
-        /// <returns></returns>
         public async Task<ActionResult<OrganizationDto>> Update(OrganizationDto orgDto)
         {
             var org = await Repository
