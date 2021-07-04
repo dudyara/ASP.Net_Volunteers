5c9db6fbeea21b7197e558cccd0a0248358be31d
Ruslan <ruslan-karamurzanov@mail.ru>
2021-07-04 21:02:11 +0700

    Исправлен фильтр Эксель для формироварния

---
+++ Volunteers/Volunteers.Services/Dto/RequestFilterExcelDto.cs
@@ -0,0 +1,31 @@
+﻿namespace Volunteers.Services.Dto
+{
+    using System;
+    using Volunteers.Entities.Enums;
+
+    /// <summary>
+    /// RequestFilterExcelDto
+    /// </summary>
+    public class RequestFilterExcelDto
+    {
+        /// <summary>
+        /// Start
+        /// </summary>
+        public DateTime Start { get; set; } = DateTime.MinValue;
+
+        /// <summary>
+        /// Final
+        /// </summary>
+        public DateTime Final { get; set; } = DateTime.MaxValue;
+
+        /// <summary>
+        /// OrganizationId
+        /// </summary>
+        public long OrganizationId { get; set; }
+
+        /// <summary>
+        /// Status
+        /// </summary>
+        public RequestStatus Status { get; set; }
+    }
+}
